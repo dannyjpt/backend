7132eb57422fb4dad142ca2da56c8373a302f12d
dannyjpt <dpuerta27@gmail.com>
2023-05-22 23:02:25 -0500

    Add files via upload

---
+++ src/models/cinema_films.js
@@ -0,0 +1,28 @@
+const {Schema, model} = require('mongoose');
+
+const cinema_filmsSchema = new Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    category: {
+        type: String,
+        require: true
+    },
+    Time: {
+        type: String,
+        require: true
+    },
+    booking_amount: {
+        type: String,
+        require: true
+    },
+    image: {
+        type: String,
+        require: true
+    }
+},{
+    timestamps: true
+});
+
+module.exports = model('cinema_films', cinema_filmsSchema);
---
+++ src/routes/cinema.routes.js
@@ -0,0 +1,28 @@
+const { Router } = require("express");
+const router = Router();
+
+const {
+  obtenerFilms,
+  filmById,
+  bookingById,
+  bookingAll,
+  allStudents,
+  registrarUsuario,
+  iniciarSesionUsuario,
+  registrarReserva,
+  actualizarBooking,
+  eliminarReserva,
+} = require("../controllers/cinema.controllers");
+
+router.get("/cinema/films", obtenerFilms);
+router.get("/cinema/films/details/:id", filmById);
+router.get("/cinema/booking/category/:id", bookingById);
+router.get("/cinema/students/:id", allStudents);
+router.get("/bookings", bookingAll);
+router.post("/cinema/register/users", registrarUsuario);
+router.post("/cinema/register/booking", registrarReserva);
+router.put('/cinema/actualizar/booking', actualizarBooking);
+router.get("/cinema/login/:email/:password", iniciarSesionUsuario);
+router.delete("/cinema/booking/eliminar/:category/:chair", eliminarReserva);
+
+module.exports = router;
