807e80d9d828dba3ec91b28d51d46a241987c8a1
dannyjpt <dpuerta27@gmail.com>
2023-05-22 23:22:04 -0500

    Update index.controllers.js

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -3,16 +3,16 @@ const indexController = {};
 
 indexController.renderIndex=(req,res) =>{
     //res.render('index');
-    res.send("hi");
+    res.send("hi pete");
 };
 
 indexController.renderProyectos=(req,res) =>{
-    res.send("hi");
+    res.send("hi p");
     //res.render('proyectos');
 };
 
 indexController.obtenerFtwo=(req,res) =>{
-    res.send("hi");
+    res.send("hi p");
     //res.render('usuarios/registro');
 };
 
